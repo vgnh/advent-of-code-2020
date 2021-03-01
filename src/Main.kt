8e0369a0968021d50abacdd3171e990297308313
Vignesh Mani <vigneshm21@protonmail.com>
2021-03-01 11:30:28 +0530

    day04

--- src/Main.kt
+++ src/Main.kt
@@ -4,6 +4,7 @@ fun main() {
     day01.main()
     day02.main()
     day03.main()
+    day04.main()
     day06.main()
 
     println("\nTime taken: ${String.format("%.6f", (System.nanoTime()-startTime)/1_000_000_000f)} seconds")
---
+++ src/day04/Day04.kt
@@ -0,0 +1,41 @@
+package day04
+
+import java.io.File
+
+const val FILENAME = "src/day04/Input.txt"
+
+val passportList = File(FILENAME).readText().split("\n\n").map { it.split("\n").joinToString(" ") }
+
+fun part01(): Int {
+    var validPass = 0
+    val reg = Regex("(?=.*byr:.*)(?=.*iyr:.*)(?=.*eyr:.*)(?=.*hgt:.*)(?=.*hcl:.*)(?=.*ecl:.*)(?=.*pid:.*)")
+    for (passport in passportList) {
+        if (reg.containsMatchIn(passport))
+            validPass++
+    }
+    return validPass
+}
+
+fun part02(): Int {
+    var validPass = 0
+    // byr = "19[2-9][0-9]|200[0-2]"
+    // iyr = "20(1[0-9]|20)"
+    // eyr = "20(2[0-9]|30)"
+    // hgt = "(1([5-8][0-9]|9[0-3])cm)|((59|6[0-9]|7[0-3])in)"
+    // hcl = "#[0-9a-f]{6}"
+    // ecl = "(amb|blu|brn|gry|grn|hzl|oth)"
+    // pid = "[0-9]{9}"
+    val reg =
+        Regex("(?=.*byr:(19[2-9][0-9]|200[0-2]).*)(?=.*iyr:(20(1[0-9]|20)).*)(?=.*eyr:(20(2[0-9]|30)).*)(?=.*hgt:((1([5-8][0-9]|9[0-3])cm)|((59|6[0-9]|7[0-3])in)).*)(?=.*hcl:(#[0-9a-f]{6}).*)(?=.*ecl:(amb|blu|brn|gry|grn|hzl|oth).*)(?=.*pid:([0-9]{9}).*)")
+    for (passport in passportList) {
+        if (reg.containsMatchIn(passport))
+            validPass++
+    }
+    return validPass
+}
+
+fun main() {
+    println("Advent of Code 2020, Day 04")
+    println(part01())
+    println(part02())
+}
