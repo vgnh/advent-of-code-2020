c5db6bf4b942026f51ad79e22f51098074b2d839
Vignesh Mani <vigneshm21@protonmail.com>
2021-03-04 11:09:21 +0530

    day12

--- src/Main.kt
+++ src/Main.kt
@@ -12,6 +12,7 @@ fun main() {
     day09.main()
     day10.main()
     day11.main()
+    day12.main()
 
     println("\nTime taken: ${String.format("%.6f", (System.nanoTime()-startTime)/1_000_000_000f)} seconds")
 }
